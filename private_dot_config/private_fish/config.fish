ffe97c532a1d775b471b545d7b939a1d5d9d1163
Lasagnanator <d96a07@gmail.com>
2026-01-22 01:06:01 +0100

    Added Fish shell configurations

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,88 @@
+### SYSTEM ###
+
+# Local path folder
+set LOCAL_PATH "$HOME/.local/bin"
+not test -d "$LOCAL_PATH" && mkdir -p "$LOCAL_PATH"
+fish_add_path "$LOCAL_PATH"
+
+# WSL
+if test -n "$WSLENV"
+    fish_add_path /mnt/c/Windows/ /mnt/c/Windows/System32 "/mnt/c/Windows/System32/WindowsPowerShell/v1.0"
+    function storePathForWindowsTerminal --on-variable PWD
+        if test -n "$WT_SESSION"
+            printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+        end
+    end
+end
+
+### ENVIRONMENT ###
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx COLORTERM truecolor
+
+### ALIASES ###
+
+if status is-interactive
+    # Utilities
+    alias v nvim
+    alias vi nvim
+    alias vim nvim
+    alias sv 'sudo -E nvim'
+    alias g git
+    alias d docker
+    alias k kubectl
+    alias t task
+    alias b bat
+    alias tf terraform
+    alias ot opentofu
+    alias lz lazygit
+    alias ldk lazydocker
+    alias cm chezmoi
+    alias open xdg-open
+    alias venv activate_venv
+    alias srsync 'rsync --rsync-path "sudo rsync"'
+
+    # Packet managers
+    alias pac 'sudo pacman'
+    alias zyp 'sudo zypper'
+    alias dnf 'sudo dnf'
+    alias apt 'sudo apt'
+end
+
+### KEYBINDS ###
+
+# TODO: add
+
+### FUNCTIONS ###
+
+function activate_venv --description "Activates Python virtual environment from current directory or git root"
+    set BASE_DIR (git rev-parse --show-toplevel 2>/dev/null)
+    if test -z "$BASE_DIR"
+        set BASE_DIR "$PWD"
+    end
+    if test -f "$BASE_DIR/.venv/bin/activate.fish"
+        source "$BASE_DIR/.venv/bin/activate.fish"
+        echo -e '\033[1;32mLoaded virtual environment!'
+    else
+        echo -e "\\033[1;31mNo virtual environment found in $BASE_DIR!"
+    end
+end
+
+### INTEGRATIONS ###
+# TODO: add integration with fzf (maybe), zoxide, direnv, rbenv and starship
+# TODO: add sourcing for NVM, Krew and SDKMAN
+
+# Kubectl
+if command -v kubectl >/dev/null 2>&1
+    set KUBECONFIGS_PATH "$HOME/.kube/config.d/"
+    not test -d "$KUBECONFIGS_PATH" && mkdir -p "$KUBECONFIGS_PATH"
+    set KUBECONFIG "$(find "$KUBECONFIGS_PATH" -type f | tr '\n' ':')"
+    set -gx KUBECONFIG
+end
+
+### DISTRO-SPECIFIC ###
+
+# Arch
+set -gx AUR_REPO_PATH '/aur'
+set -gx AUR_DB_PATH "$AUR_REPO_PATH/aur.db"
